d56618bdf69d74d25247b20646dadf4c80de913f
Sujee <info@29.works>
2022-03-26 22:11:46 +0900

    smooth scroll

--- support/special/facilitytype/js/facilitytype.js
+++ support/special/facilitytype/js/facilitytype.js
@@ -64,3 +64,14 @@ $(window).scroll(function () {
 		$('.floating').fadeOut(300);
 	}
 });
+
+// smooth scroll
+$(function () {
+	$('a[href^="#"]').click(function () {
+		var href = $(this).attr('href');
+		var target = $(href == '#' || href == '' ? 'html' : href);
+		var position = target.offset().top;
+		$('html, body').animate({ scrollTop: position }, 500, 'swing');
+		return false;
+	});
+});
